7d4d741ac0106e30a78bdd63fb496fab27d7904a
Renato Naper <r.naper@spotpromo.com.br>
2019-10-06 08:18:50 -0300

    Criando o objeto Spot

--- src/controllers/SessionController.js
+++ src/controllers/SessionController.js
@@ -1,7 +1,18 @@
 //index (lista de sessoes), show (listar uma sessao), store (criar), update, destroy(deletar)
-
+const User = require('../models/User');
 module.exports= {
-    store(req,res){
-        return res.json({message: "Hello World"});
+    async store(req,res)
+    {
+        //return res.json({message: "Hello World"});
+        //quando {} na declaracao  da variavel  ele está falando que é eu usar a variavel para buscar dentro do body
+        //colocando o await na frente da chamada ele vai entender que ele só prosseguir quando terminar a chamada 
+        const {email} = req.body; 
+        //const user = await User.create({email});
+        // como o mesmo nome da variavel é da chave fica mais fácil
+        let user = await User.findOne({email}); 
+        if (!user){
+            user = await User.create({email});
+        }
+        return res.json(user);
     }
 };
--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const SessionController = require('./controllers/SessionController');
+const SpotController = require('./controllers/SpotController');
 const routes = express.Router();
 
-
-routes.post('/users',SessionController.store);
+routes.post('/sessions',SessionController.store);
+routes.post('/spots',SpotController.store);
 
 module.exports = routes;
