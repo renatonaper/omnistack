71c00f413b2d7b31b5e0bdd903d3ccfe703d8e0f
Renato Naper <r.naper@spotpromo.com.br>
2019-10-06 20:30:31 -0300

    finalizando o backend

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const multer = require('multer');
+const uploadConfig = require('./config/Upload');
+
 const SessionController = require('./controllers/SessionController');
 const SpotController = require('./controllers/SpotController');
+const DashboardController = require('./controllers/DashboardController')
+const BookingController = require('./controllers/BookingController')
+
+
 const routes = express.Router();
+const updload = multer(uploadConfig);
 
 routes.post('/sessions',SessionController.store);
-routes.post('/spots',SpotController.store);
+routes.get('/spots',SpotController.index);
+routes.post('/spots',updload.single('thumbnail'),SpotController.store);
+routes.get('/dashboard',DashboardController.show);
 
+routes.post('/spots/:spot_id/bookings',BookingController.store);
 module.exports = routes;
